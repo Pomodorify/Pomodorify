32fe171288975c0f4c5d6b37b50812bcaff114fd
Norbert Koziana <koziana11norbert@gmail.com>
2024-05-25 15:29:15 +0200

    Added minutes label when displaying statistics

--- app/src/main/java/com/example/pomodorify/statistics/ListStatisticsAdapter.java
+++ app/src/main/java/com/example/pomodorify/statistics/ListStatisticsAdapter.java
@@ -39,7 +39,7 @@ public class ListStatisticsAdapter extends RecyclerView.Adapter<ListStatisticsVi
             activity = "Focus";
 
         holder.getItemActivity().setText(activity);
-        holder.getItemDuration().setText("Duration: " + statisticsRecordCard.get(position).getStatisticsRecord().getTime());
+        holder.getItemDuration().setText("Duration: " + statisticsRecordCard.get(position).getStatisticsRecord().getTime() + "minutes");
         holder.getItemDate().setText("Date: " + statisticsRecordCard.get(position).getStatisticsRecord().getFormattedDate());
     }
     @Override
